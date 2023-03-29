41c78d1734555c69ba460b4a8d4af4813e014a2a
nriin-rizeki <ipandani2505@gmail.com>
2023-03-29 17:07:26 +0700

    fix: add eslint, prettier

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import './app.css'
-import App from './App.svelte'
+import './app/App.app.css';
+import App from './app/App.app.svelte';
 
 const app = new App({
   target: document.getElementById('app'),
-})
+});
 
-export default app
+export default app;
