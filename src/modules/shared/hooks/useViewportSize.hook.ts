1fa46ed307cd1d0110c638b7da741bd2c66e9e2b
nriin-rizeki <ipandani2505@gmail.com>
2023-05-31 10:39:30 +0700

    feat: useViewportSize

---
+++ src/modules/shared/hooks/useViewportSize.hook.ts
@@ -0,0 +1,39 @@
+import { writable, type Writable } from 'svelte/store';
+import { defaultWindow, environment } from '../constants/global.constant';
+
+const listenerOptions = {
+  passive: true,
+};
+
+/**
+ * get access to the current with and height of the viewport, and it also subscribes to viewport changes.
+ *
+ * @example
+ *
+ * const viewport = useViewportSize();
+ *
+ * $: ({ width, height } = $viewport);
+ */
+export function useViewportSize(): Writable<{ width: number; height: number }> {
+  let window: Window;
+  if (environment.browser) window = defaultWindow;
+
+  const windowSize = writable({
+    width: environment.browser ? window.innerWidth : 0,
+    height: environment.browser ? window.innerHeight : 0,
+  });
+
+  const setSize = () => {
+    windowSize.set({
+      width: window.innerWidth || 0,
+      height: window.innerHeight || 0,
+    });
+  };
+
+  if (environment.browser) {
+    window.addEventListener('resize', setSize, listenerOptions);
+    window.addEventListener('orientationchange', setSize, listenerOptions);
+  }
+
+  return windowSize;
+}
