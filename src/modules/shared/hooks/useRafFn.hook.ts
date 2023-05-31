da492fd81417b9808816f763396d831a4766dc35
nriin-rizeki <ipandani2505@gmail.com>
2023-05-31 10:36:36 +0700

    feat: useRafFn

---
+++ src/modules/shared/hooks/useRafFn.hook.ts
@@ -0,0 +1,68 @@
+import { environment } from '../constants/global.constant';
+import type { ConfigurableWindow, Pauseable } from '../types/action.type';
+
+export interface RafFnOptions extends ConfigurableWindow {
+  /**
+   * Start the requestAnimationFrame loop immediately on creation
+   *
+   * @default true
+   */
+  immediate?: boolean;
+}
+
+/**
+ * Call function on every `requestAnimationFrame`. With controls of pausing and resuming.
+ *
+ * @param fn callback function
+ * @param options controls whether the callback is triggered immediately
+ *
+ * @example
+ *
+ * ```tsx
+ * <script>
+ * 	let count = 0;
+ * 	const { pause, resume } = useRafFn(() => {
+ * 		count++
+ *     })
+ * </script>
+ *
+ * Count: {count}
+ * <button on:click={() => pause()}>Pause</button>
+ * <button on:click={() => resume()}>Resume</button>
+ * ```
+ */
+export function useRafFn(fn: () => void, options: RafFnOptions = {}): Pauseable {
+  const { immediate = true } = options;
+  let isActive = false;
+
+  function loop() {
+    if (!isActive) return;
+
+    try {
+      fn();
+
+      if (environment.browser) window.requestAnimationFrame(loop);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  function resume() {
+    if (!isActive) {
+      isActive = true;
+      loop();
+    }
+  }
+
+  function pause() {
+    isActive = false;
+  }
+
+  if (immediate) resume();
+
+  return {
+    isActive,
+    pause,
+    resume,
+  };
+}
--- src/modules/shared/types/action.type.ts
+++ src/modules/shared/types/action.type.ts
@@ -28,3 +28,48 @@ export interface Environment {
   readonly browser?: boolean | Fn2<never, boolean>;
   readonly server?: boolean | Fn2<never, boolean>;
 }
+
+export interface Pauseable {
+  /**
+   * A state value that indicates whether a pauseable instance is active
+   */
+  isActive: boolean;
+
+  /**
+   * Temporary pause the effect from executing
+   */
+  pause: VoidFunction;
+
+  /**
+   * Resume the effects
+   */
+  resume: VoidFunction;
+}
+
+export interface ConfigurableWindow {
+  /*
+   * Specify a custom `window` instance, e.g. working with iframes or in testing environments.
+   */
+  window?: Window;
+}
+
+export interface ConfigurableDocument {
+  /*
+   * Specify a custom `document` instance, e.g. working with iframes or in testing environments.
+   */
+  document?: Document;
+}
+
+export interface ConfigurableNavigator {
+  /*
+   * Specify a custom `navigator` instance, e.g. working with iframes or in testing environments.
+   */
+  navigator?: Navigator;
+}
+
+export interface ConfigurableLocation {
+  /*
+   * Specify a custom `location` instance, e.g. working with iframes or in testing environments.
+   */
+  location?: Location;
+}
