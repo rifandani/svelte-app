c11e7fde2e409a276f12494f2bb3ac23e2271309
nriin-rizeki <ipandani2505@gmail.com>
2023-05-31 22:54:52 +0700

    chore: setup test

---
+++ src/setupTests.ts
@@ -0,0 +1,26 @@
+import matchers from '@testing-library/jest-dom/matchers';
+import { expect } from 'vitest';
+// import { server } from './mocks/http/server.http';
+// import './mocks/module.mock';
+
+expect.extend(matchers);
+
+// Establish API mocking before all tests with MSW
+beforeAll(() => {
+  // server.listen({
+  //   onUnhandledRequest: 'warn',
+  // });
+});
+
+// Reset any request handlers that we may add during the tests, so they don't affect other tests.
+afterEach(() => {
+  // server.resetHandlers();
+  // vi.resetAllMocks();
+  // vi.restoreAllMocks();
+  // vi.clearAllMocks();
+});
+
+// Clean up after the tests are finished.
+afterAll(() => {
+  // server.close()
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte({ hot: !process.env.VITEST })], // disabling Svelte's hot module reload when tests are running
+  test: {
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['html'],
+    environment: 'jsdom', // mocking the DOM API
+    globals: true,
+    setupFiles: ['node_modules/@testing-library/jest-dom/extend-expect.js', 'src/setupTests.ts'],
+    // Will call .mockRestore() on all spies before each test. This will clear mock history and reset its implementation to the original one.
+    restoreMocks: true,
+    coverage: {
+      provider: 'c8', // 'istanbul' / 'c8'
+      reporter: ['text', 'json', 'html'],
+      statements: 80,
+      branches: 80,
+      functions: 65,
+      lines: 80,
+      exclude: [
+        'coverage/**',
+        'dist/**',
+        'packages/*/test{,s}/**',
+        '**/*.d.ts',
+        'cypress/**',
+        'test{,s}/**',
+        'test{,-*}.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/__tests__/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/.{eslint,mocha,prettier}rc.{js,cjs,yml}',
+        // above is default
+        'src/setupTests.ts',
+        'src/main.ts',
+        'src/mocks/**',
+        'src/assets/**',
+        'src/i18n/**',
+      ],
+    },
+  },
 });
