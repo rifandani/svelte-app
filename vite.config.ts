52393c689be60022550888ab2745ce45fffa0700
nriin-rizeki <ipandani2505@gmail.com>
2023-06-10 23:24:26 +0700

    chore: bump deps

--- vite.config.ts
+++ vite.config.ts
@@ -8,19 +8,21 @@ export default defineConfig({
   plugins: [svelte({ hot: !process.env.VITEST })], // disabling Svelte's hot module reload when tests are running
   test: {
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    // to see how your tests are running in real time in the terminal, add "default"
+    // to generate HTML output and preview the results of your tests, add "html"
     reporters: ['default', 'html'],
     environment: 'jsdom', // mocking the DOM API
-    globals: true,
+    globals: true, // use APIs globally like jest
     setupFiles: ['node_modules/@testing-library/jest-dom/extend-expect.js', 'src/setupTests.ts'],
     // Will call .mockRestore() on all spies before each test. This will clear mock history and reset its implementation to the original one.
     restoreMocks: true,
     coverage: {
-      provider: 'c8', // 'istanbul' / 'c8'
+      provider: 'istanbul', // 'istanbul' / 'v8'
       reporter: ['text', 'json', 'html'],
-      statements: 80,
-      branches: 80,
-      functions: 65,
-      lines: 80,
+      statements: 50,
+      branches: 50,
+      functions: 50,
+      lines: 50,
       exclude: [
         'coverage/**',
         'dist/**',
